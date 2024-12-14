6fd34387140972f9ed6dc818c134bd9de428ef1f
ASSASSIN <ziadabobakr@gmail.com>
2024-12-14 02:07:40 +0200

    pages and build deployment

--- app/sections/Contact.jsx
+++ app/sections/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
             <div className="w-full h-full">
                 <h1 className="text-5xl font-semibold text-[#ffb703] p-10">Contact:</h1>
                 <div className="flex w-full mx-auto my-5">
-                    <ul className="flex flex-wrap gap-16 text-center p-5 py-6 font-semibold text-3xl text-blue-100 justify-center">
+                    <ul className="flex flex-wrap gap-16 text-center p-5 py-6 font-semibold text-3xl max-md:text-2xl text-blue-100 justify-center">
                         <li className="bg-blue-200 rounded-2xl shadow-2xl p-8 transition-all hover:-translate-y-8 cursor-pointer">
                             <h1 className="pb-5">Name</h1>
                             <h4 className="text-[#ffb703]">Mohamed Abobakr</h4>
